4ccd4c53d84d0e14e3ece8abaa51f8291d48556a
simon 95 <tbobur1995@gmail.com>
2024-10-24 16:21:27 +0900

    fix: modify for simple deployment

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # PRODUCTION
-git checkout master
+# git checkout master
 
-npm i yarn -g
-yarn global add serve
-yarn
-yarn run build
-pm2 start "yarn run start:prod" --name=BooksShop-React
+# npm i yarn -g
+# yarn global add serve
+# yarn
+# yarn run build
+pm2 start "npm run start" --name=ROIP-Frontend
 
 # DEVELOPMENT
 # npm i yarn -g
